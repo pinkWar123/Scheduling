bd0f17393a52f4a1c7dd2a5681321bce9a765abb
khanhst0209 <phamnguyenkhanhst21@gmail.com>
2024-01-26 23:18:32 +0700

    bo sung

--- RoundRobin.cpp
+++ RoundRobin.cpp
@@ -28,10 +28,48 @@ void RoundRobin::Run()
                 if(!temp.ResourceBurstTime.empty())
                     ioQueue.push(temp);
                 cpuQueue.pop();
+                count = 0;
             }
             else{
+                ++count;
+                if(count == time_quantum)
+                {
+                    cpuQueue.pop();
+                    cpuQueue.push(temp);
+                    count = 0;
+                }
                 CPUsheduling.push_back(temp.ID);
             }
         }
+        else
+            CPUsheduling.push_back(-1);
+
+            
+        if(!ioQueue.empty())
+        {
+            Process temp = ioQueue.front();
+            --temp.ResourceBurstTime[0];
+            if(temp.ResourceBurstTime[0] == 0)
+            {
+                temp.ResourceBurstTime.erase(temp.ResourceBurstTime.begin());
+                if(!temp.CPUBurstTime.empty())
+                    cpuQueue.push(temp);
+                ioQueue.pop();
+            }
+            else
+            {
+                Resourcesheduling.push_back(temp.ID);
+            }
+        }
+        else if(ioQueue.empty())
+        {
+            Resourcesheduling.push_back(-1);
+        }
+
+        if(allIsDone && cpuQueue.empty() && ioQueue.empty())
+        {
+            break;
+        }
+        currentTime++;
     }
 }
